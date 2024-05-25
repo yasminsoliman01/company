f4949614a8e9bd137d4733861119e083ed238ddd
yasminsoliman01 <yasminsoliman825@gmail.com>
2024-05-25 19:27:43 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+let slides = document.querySelectorAll('.reviews .flex .slides-container .slide');
+let index =0;
+
+function next(){
+    slides[index].classList.remove('active');
+    index =(index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev(){
+    slides[index].classList.remove('active');
+    index =(index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+
+
+let monthBtn = document.querySelector('.pricing .toggle-buttons .month-btn');
+let yearBtn = document.querySelector('.pricing .toggle-buttons .year-btn');
+let monthlyplan = document.querySelectorAll('.pricing .box-container .box .month');
+let yearlyPlan = document.querySelectorAll('.pricing .box-container .box .year');
+
+yearBtn.onclick = () => {
+    monthBtn.classList.remove('active');
+    yearBtn.classList.add('active');
+
+    monthlyplan.forEach(mo => { mo.style.display = 'none' });
+    yearlyPlan.forEach(yr => { yr.style.display = 'block' });
+}
+
+
+
+monthBtn.onclick = () => {
+    monthBtn.classList.add('active');
+    yearBtn.classList.remove('active');
+
+    monthlyplan.forEach(mo => { mo.style.display = 'block' });
+    yearlyPlan.forEach(yr => { yr.style.display = 'none' });
+}
+
+
+
+
